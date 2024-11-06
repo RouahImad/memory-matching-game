99b5929fb8496993b3dffed1b0ba05ccfa4b0134
orcas-6 <irouah2@gmail.com>
2024-11-06 23:22:50 +0100

    added images instead of online urls

--- src/components/BoardContainer.jsx
+++ src/components/BoardContainer.jsx
@@ -20,56 +20,56 @@ const BoardContainer = ({ setCount }) => {
     }
 
     const urls = [
-        "https://t4.ftcdn.net/jpg/00/57/13/63/240_F_57136319_lcUZ6ETec1DbJMcSXPDdWgB8xFaYDOwp.jpg",
-        "https://t4.ftcdn.net/jpg/02/66/68/37/240_F_266683754_wzx9XxeNXKudct2Q3qwQf1PvVaQaKOf6.jpg",
-        "https://t3.ftcdn.net/jpg/01/91/55/42/240_F_191554261_Kfn9mPkFP2lRmNuSkLkoe6yMnOdC1GUC.jpg",
-        "https://t3.ftcdn.net/jpg/00/91/30/42/240_F_91304200_GWKgCZTgxvN7KCNTExaXB6CfyvxGpT6v.jpg",
-        "https://t4.ftcdn.net/jpg/00/53/59/03/240_F_53590332_GktsdggabKrEv4a3URfSalFHofBoFOqL.jpg",
-        "https://t3.ftcdn.net/jpg/00/78/37/74/240_F_78377459_YZ2gxe46PJmgtpXjeHC5l6qQOLtIfpe3.jpg",
-        "https://t3.ftcdn.net/jpg/00/52/44/64/240_F_52446492_vKWvqM8QhyhYhaz4EHL881Dher7x092m.jpg",
-        "https://t3.ftcdn.net/jpg/03/58/94/72/240_F_358947207_bOEQt74sKb6x6ONbNnJZpPDtuymVT6Dv.jpg",
+        "/src/assets/pumpkin.jpg",
+        "/src/assets/greenThing.jpg",
+        "/src/assets/onion.jpg",
+        "/src/assets/redThing.jpg",
+        "/src/assets/letus.jpg",
+        "/src/assets/purpleThing.jpg",
+        "/src/assets/tomato.jpg",
+        "/src/assets/darkRedThing.jpg",
     ];
     const [randUrl, setRandUrl] = useState([]);
-    const [clickCount, setClickCount] = useState(1);
     const [flippedCards, setFlippedCards] = useState([]);
     const [matchedCards, setMatchedCards] = useState([]);
+    const [clickCount, setClickCount] = useState(0);
 
     useEffect(() => {
         setRandUrl(shuffle([...urls, ...urls]));
     }, []);
 
     const handleClick = (e, id) => {
-        console.log(flippedCards);
-        if (
-            matchedCards.some((item) => item.id === id) ||
-            matchedCards.some((item) => item.id === id)
-        )
+        // console.log(matchedCards);
+        // console.log(matchedCards.some((item) => item.id === id));
+
+        if (clickCount > 2 || matchedCards.some((item) => item.id === id))
             return;
 
-        if (clickCount <= 2) {
-            setClickCount((old) => old + 1);
-            setCount((old) => old + 1);
-            e.target.src = randUrl[id];
-            setFlippedCards((prev) => [...prev, { id, url: randUrl[id] }]);
+        e.target.src = randUrl[id];
 
-            setTimeout(() => {
-                setFlippedCards(flippedCards.filter((el) => el[0] != id));
-                e.target.src = QuestionCard;
-                setClickCount((old) => old - 1);
-            }, 4000);
-        }
+        setFlippedCards((prev) => [...prev, { id, url: randUrl[id] }]);
+        setCount((old) => old + 1);
+        setClickCount((old) => old + 1);
+
+        let timer = setTimeout(() => {
+            e.target.src = QuestionCard;
+            setFlippedCards(flippedCards.filter((el) => el[0] != id));
+            setClickCount((old) => old - 1);
+        }, 1500);
 
         if (flippedCards.length >= 2) {
-            if (flippedCards[0][1] == randUrl[id]) {
+            console.log(flippedCards[0]);
+
+            if (flippedCards[0].url == randUrl[id]) {
+                clearTimeout(timer);
                 console.log("found same");
                 e.target.src = randUrl[id];
-                setMatchedCards(
-                    (prev) => [...prev, { id, url: randUrl[id] }],
-                    (prev) => [
-                        ...prev,
-                        { id: flippedCards[0][0], url: flippedCards[0][1] },
-                    ]
-                );
+
+                setMatchedCards((prev) => [...prev, { id, url: randUrl[id] }]);
+                setMatchedCards((prev) => [
+                    ...prev,
+                    { id: flippedCards[0].id, url: flippedCards[0].url },
+                ]);
             }
         }
     };
